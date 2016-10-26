23d712f9051f7d4257c3a6716c574be753a4d54e
刘强 <liuqiang@kaike.la>
2016-10-26 15:15:49 +0800

    integer

---
+++ src/main/java/Int.java
@@ -0,0 +1,12 @@
+
+public class Int {
+	public static void main(String[] args) {
+		Integer a=new Integer(1);
+		
+		Integer b=new Integer(1);
+		
+		if(a==b){
+			System.out.println("true");
+		}
+	}
+}
--- src/main/java/com/github/ichenkaihua/TestRegex.java
+++ src/main/java/com/github/ichenkaihua/TestRegex.java
@@ -1,12 +1,3 @@
-/**
- * 版权所有@: 杭州铭师堂教育科技发展有限公司
- * 创建时间: 2016年8月2日下午4:31:17
- * 注意：本内容仅限于杭州铭师堂教育科技发展有限公司内部使用，禁止外泄以及用于其他的商业目的
- * CopyRight@: 2016 Hangzhou Mistong Educational Technology Co.,Ltd.
- * All Rights Reserved.
- * Note:Just limited to use by Hangzhou Mistong Educational Technology Co.,Ltd. Others are forbidden. 
- * Created on: 2016年8月2日下午4:31:17
- */
 package com.github.ichenkaihua;
 
 import java.util.regex.Matcher;
@@ -20,18 +11,18 @@ import java.util.regex.Pattern;
  */
 public class TestRegex {
 	
-	private final static String RESOURCEIDPATTERN = "(data-auth)\\s{0,}=\\s{0,}[\'\"]\\S+[\'\"]";
+	private final static String RESOURCE_ID_PATTERN = "(data-auth)\\s{0,}=\\s{0,}[\'\"]\\S+[\'\"]";
 
-	private final static String RESOURCECODEPATTERN = "[\'\"]\\S+[\'\"]";
+	private final static String RESOURCE_CODE_PATTERN = "[\'\"]\\S+[\'\"]";
 
 	private final static String IDTOKEN = "[\'\"]";
 	
 	private String getAuthResourceCode(String content) {
-		Pattern pId = Pattern.compile(RESOURCEIDPATTERN,Pattern.CASE_INSENSITIVE);
+		Pattern pId = Pattern.compile(RESOURCE_ID_PATTERN,Pattern.CASE_INSENSITIVE);
 		Matcher mId = pId.matcher(content);
 		if (mId.find()) {
 			String resourceId = mId.group();
-			Pattern pCode = Pattern.compile(RESOURCECODEPATTERN);
+			Pattern pCode = Pattern.compile(RESOURCE_CODE_PATTERN);
 			Matcher mCode = pCode.matcher(resourceId);
 			if (mCode.find()) {
 				String resourceTemp = mCode.group();
